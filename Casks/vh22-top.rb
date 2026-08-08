ed47c3adda55ac5f74f9c61ee1a83243a2ef28c7
Brandon Lehman <aelder@gmail.com>
2026-08-07 18:42:08 -0700

    Use compatible binary cask for v1.1.2

--- Casks/vh22-top.rb
+++ Casks/vh22-top.rb
@@ -1,6 +1,6 @@
 cask "vh22-top" do
-  version "1.1.1"
-  sha256 "d89e48e4604d6c64bf43f7e2b01d92a6f1570582a644f5b334e56f98cdba2379"
+  version "1.1.2"
+  sha256 "5530ad8430a0f240ce2389fd88c906df774575d1dd2a3692d0c9c3e354ef42fd"
 
   url "https://github.com/aelder/walled-garden-hasher/releases/download/v#{version}/walled-garden-hasher-v#{version}-macos-arm64.tar.gz"
   name "The Walled Garden Hasher"
@@ -10,9 +10,7 @@ cask "vh22-top" do
   depends_on arch: :arm64
   depends_on macos: :sequoia
 
-  command_wrapper "vh22-top",
-                  executable: "#{staged_path}/walled-garden-hasher-v#{version}-macos-arm64/vh22-top",
-                  env:        { "VH22_NEWS" => "#{staged_path}/walled-garden-hasher-v#{version}-macos-arm64/news.md" }
+  binary "walled-garden-hasher-v#{version}-macos-arm64/vh22-top"
 
   caveats <<~EOS
     vh22-top is beta software provided without warranty. Mining places the
